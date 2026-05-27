7d60a4c049e94b1e359f2b0f117f9536967447cf
CarlosDev <carloseliaspinto96@gmail.com>
2026-05-27 14:39:28 -0400

    recorriendo ABC + imagenes

--- script.js
+++ script.js
@@ -25,3 +25,48 @@ titulos.forEach(function(titulo) {
     })
 
 })
+
+// probando html
+const abecedario = [
+    { letra: "A", imageN: "imagenes/a.jpg" },
+    { letra: "B", imagen: "imagenes/b.jpg" },
+    { letra: "C", imagen: "imagenes/c.jpg" },
+    { letra: "D", imagen: "imagenes/d.jpg" },
+    { letra: "E", imagen: "imagenes/e.jpg" },
+    { letra: "F", imagen: "imagenes/f.jpg" },
+    { letra: "G", imagen: "imagenes/g.jpg" },
+    { letra: "H", imagen: "imagenes/h.jpg" },
+    { letra: "I", imagen: "imagenes/i.jpg" },
+    { letra: "J", imagen: "imagenes/j.jpg" },
+    { letra: "K", imagen: "imagenes/k.jpg" },
+    { letra: "L", imagen: "imagenes/l.jpg" },
+    { letra: "M", imagen: "imagenes/m.jpg" },
+    { letra: "N", imagen: "imagenes/n.jpg" },
+    { letra: "O", imagen: "imagenes/o.jpg" },
+    { letra: "P", imagen: "imagenes/p.jpg" },
+    { letra: "Q", imagen: "imagenes/q.jpg" },
+    { letra: "R", imagen: "imagenes/r.jpg" },
+    { letra: "S", imagen: "imagenes/s.jpg" },
+    { letra: "T", imagen: "imagenes/t.jpg" },
+    { letra: "U", imagen: "imagenes/u.jpg" },
+    { letra: "V", imagen: "imagenes/v.jpg" },
+    { letra: "W", imagen: "imagenes/w.jpg" },
+    { letra: "X", imagen: "imagenes/x.jpg" },
+    { letra: "Y", imagen: "imagenes/y.jpg" },
+    { letra: "Z", imagen: "imagenes/z.jpg" }
+]
+
+const contenedor = document.querySelector("#contenedor-letras")
+
+abecedario.forEach(item => {
+
+    const tarjeta = document.createElement("section")
+    tarjeta.classList.add("tarjeta-letra")
+
+    tarjeta.innerHTML = `
+        <h3>Letra ${item.letra}</h3>
+        <img src="${item.imagen}" alt="Imagen ${item.letra}" width="200">
+    `
+
+    contenedor.appendChild(tarjeta)
+})
